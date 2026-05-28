92fa13e0fb35b514a3b9b058afc0a90bae5d1308
imarp8 <arpitchourasia64@gmail.com>
2026-05-29 00:14:51 +0530

    Update MLP from scratch (Build a Neural Net 4/5)

--- foundations/mlp.py
+++ foundations/mlp.py
@@ -17,9 +17,4 @@ class Solution:
             else:
                 a = np.maximum(0,z)
 
-
-        # weights: list of 2D weight matrices
-        # biases: list of 1D bias vectors
-        # Apply ReLU after each hidden layer, no activation on output layer
-        # return np.round(your_answer, 5)
         pass
